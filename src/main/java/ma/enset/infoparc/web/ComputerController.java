e50718e6a05c422f8710e039a70d1f2091eeb2e8
d-sar <damrisara1@gmail.com>
2025-04-19 11:39:59 +0100

    Computercontroller

--- src/main/java/ma/enset/infoparc/web/ComputerController.java
+++ src/main/java/ma/enset/infoparc/web/ComputerController.java
@@ -1,11 +1,38 @@
 package ma.enset.infoparc.web;
 
+import ma.enset.infoparc.entities.Computer;
 import ma.enset.infoparc.service.ComputerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class ComputerController {
    @Autowired
-   private ComputerService computerService;
+   private  ComputerService computerService;
+
+   @GetMapping("index")
+   public String index(Model model,
+                       @RequestParam(defaultValue = "") String keyword,
+                       @RequestParam(defaultValue = "0") int page,
+                       @RequestParam(defaultValue = "5") int size){
+      Page<Computer> computerPage = computerService.getComputers(keyword, page, size);
+      model.addAttribute("listcomputers", computerPage.getContent());
+      model.addAttribute("currentPage",page);
+      model.addAttribute("totalPages",computerPage.getTotalPages());
+      model.addAttribute("keyword", keyword);
+
+      return "computers";
+
+   }
+   @GetMapping("/deleteComputer")
+   public String deleteComputer(@RequestParam Long id ,@RequestParam int page,@RequestParam String keyword){
+      computerService.deleteComputer(id);
+      return "redirect:/index?page="+page+"&keyword="+keyword;
+   }
+
 }
